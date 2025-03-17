2d5282fb20ce51bb56d8fda593858e6c1903b176
Taso Perdikoulias <tperdis@gmail.com>
2025-03-16 21:44:27 -0400

    update text

--- src/main/java/com/example/cicdpipeline/web/TestController.java
+++ src/main/java/com/example/cicdpipeline/web/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<Map<String,String>> test() {
-        return ResponseEntity.ok().body(Map.of("Testing Now", "Up an Running"));
+        return ResponseEntity.ok().body(Map.of("Testing Now!!!", "Up an Running"));
 
     }
 
