b97f3c2c2bf348750bd4ef941f30da9df44250aa
Taso Perdikoulias <tperdis@gmail.com>
2025-03-16 20:18:29 -0400

    testing commit

--- src/test/java/com/example/firstjobapp/FirstjobappApplicationTests.java
+++ src/test/java/com/example/firstjobapp/FirstjobappApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.cicdpipeline;
+package com.example.firstjobapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CicdpipelineApplicationTests {
+class FirstjobappApplicationTests {
 
 	@Test
 	void contextLoads() {
